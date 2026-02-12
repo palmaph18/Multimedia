76e704cad223333bf19082f1c2826c08be6ecd91
Jesus Reynaldo Palma <115763443+palmaph18@users.noreply.github.com>
2026-02-12 15:59:37 -0600

    Add binary filter for volcanic image processing
    Implement binary filter for volcanic image, converting pixels to black and white.

---
+++ Practicas/Procesamiento de imagenes/Filtro binario Volcan/main.py
@@ -0,0 +1,26 @@
+#Binary filter: Black and White for Volcan
+
+file = open('./images/volcan.bmp','rb')
+fileo = open('./images/volcanbin.bmp','wb')
+metadata = file.read(54)
+fileo.write(metadata)
+blanco = [0xff,0xff,0xff]
+negro = [0x00,0x00,0x00]
+
+file.seek(54,0)
+no_pix = 0
+limite = (pow(2, 24)-1)/2
+while(True):
+    pixel_data = file.read(3)
+    if(len(pixel_data) > 0):
+        valor_int = int.from_bytes(bytes(pixel_data),byteorder='little')
+        if(valor_int<limite):
+            fileo.write(bytes(blanco))
+        else:
+            fileo.write(bytes(negro))
+        no_pix += 1
+    else:
+        break
+print('No Pixels: '+str(no_pix))
+file.close()
+fileo.close()
